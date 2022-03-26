c23223220bb6ccbb65065c21df231bbc88637439
aulneau <aulneau@users.noreply.github.com>
2022-03-26 12:48:14 -0500

    fix: add 404 fallback

--- src/components/global-app/routes.tsx
+++ src/components/global-app/routes.tsx
@@ -1,6 +1,8 @@
 import { Route } from 'wouter';
 import { Home } from '../../views/home';
 import { Document } from '../../views/document';
+import { useAuth } from '@micro-stacks/react';
+import { Suspense } from 'react';
 
 export enum RoutePaths {
   Home = '/',
@@ -8,10 +10,11 @@ export enum RoutePaths {
 }
 
 export const Routes = () => {
+  const { isSignedIn } = useAuth();
   return (
-    <>
+    <Suspense fallback={<></>}>
       <Route path={RoutePaths.Home} component={Home} />
-      <Route path={RoutePaths.Document} component={Document} />
-    </>
+      <Route path={RoutePaths.Document} component={isSignedIn ? Document : Home} />
+    </Suspense>
   );
 };
--- src/views/document.tsx
+++ src/views/document.tsx
@@ -1,13 +1,38 @@
 import { Editor } from '../components/editor/editor';
-import { useRoute } from 'wouter';
+import { Link, useRoute } from 'wouter';
 import { RoutePaths } from '../components/global-app/routes';
 import { Suspense } from 'react';
-import { Stack } from '@nelson-ui/react';
+import { Box, Stack } from '@nelson-ui/react';
 import { TitleEditor } from '../components/editor/document-title-editor';
+import { useDocuments } from '../store/documents';
+
+const NotFoundView = () => {
+  return (
+    <Stack
+      pb={'128px'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      flexGrow={1}
+      textAlign={'center'}
+    >
+      <Box fontSize={'$6'}>Can't find this document...</Box>
+      <Box lineHeight={'1.65'} maxWidth={'42ch'}>
+        Strange, we can't seem to find this document. Why not go home and make a new one?
+      </Box>
+      <Link href={RoutePaths.Home}>
+        <button>Go home!</button>
+      </Link>
+    </Stack>
+  );
+};
 
 export const Document = () => {
+  const [state] = useDocuments();
   const [, params] = useRoute(RoutePaths.Document);
   if (!params) return null;
+  const docId = params.doc_id;
+  const docExists = state?.documents[docId];
+  if (!docExists) return <NotFoundView />;
   return (
     <Stack
       alignItems={'stretch'}
